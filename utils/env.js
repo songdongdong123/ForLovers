0637ff9104dcbd64a06e0fa56246ca10ef18c844
ethan <172529131@qq.com>
2018-11-16 21:02:00 +0800

    add some pages

--- utils/env.js
+++ utils/env.js
@@ -2,13 +2,13 @@ const isDev = true
 let config = {}
 if(isDev) {
   config = {
-    appid: 'wx2c3a4a9b6f325bb4',
+    appid: '',
     webView: 'https://www.ethansblogs.com',
     url: 'https://www.ethansblogs.com'
   }
 } else {
   config = {
-    appid: 'wx2c3a4a9b6f325bb4',
+    appid: '',
     webView: 'https://www.ethansblogs.com',
     url: 'https://www.ethansblogs.com'
   }
